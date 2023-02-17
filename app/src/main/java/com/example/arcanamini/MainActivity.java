81ba41f3a6cb3b42521dae78fd99af3d4983a613
nethen <72537484+nethen@users.noreply.github.com>
2023-02-17 13:59:10 -0800

    Updated toast

--- app/src/main/java/com/example/arcanamini/MainActivity.java
+++ app/src/main/java/com/example/arcanamini/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.arcanamini;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hi jasper", Toast.LENGTH_SHORT).show();
     }
 }
