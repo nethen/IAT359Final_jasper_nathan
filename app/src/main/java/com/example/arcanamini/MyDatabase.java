24c839edbb080e3ad2bac2f8b4ba04094b00131a
jaspergass <115849451+jaspergass@users.noreply.github.com>
2023-02-18 11:44:36 -0800

    Database updates
    The recycler should be showing the database, but I do not think its being created. the helper should toast when it calls its OnCreate() but there is no toast. I think the issue is in RecyclerActivity ?

--- app/src/main/java/com/example/arcanamini/MyDatabase.java
+++ app/src/main/java/com/example/arcanamini/MyDatabase.java
@@ -35,30 +35,6 @@ public class MyDatabase {
         return cursor;
     }
 
-
-    public String getSelectedData(String suit)
-    {
-        //select plants from database of type 'herb'
-        SQLiteDatabase db = helper.getWritableDatabase();
-        String[] columns = {Constants.NAME, Constants.SUIT, Constants.STATUS};
-
-        String selection = Constants.SUIT + "='" +suit+ "'";  //Constants.TYPE = 'type'
-        Cursor cursor = db.query(Constants.MINI_TABLE, columns, selection, null, null, null, null);
-
-        StringBuffer buffer = new StringBuffer();
-        while (cursor.moveToNext()) {
-
-            int index1 = cursor.getColumnIndex(Constants.NAME);
-            int index2 = cursor.getColumnIndex(Constants.SUIT);
-            int index3 = cursor.getColumnIndex(Constants.STATUS);
-            String cardName = cursor.getString(index1);
-            String cardSuit = cursor.getString(index2);
-            String cardStatus = cursor.getString(index3);
-            buffer.append(cardName + "," + cardSuit + "," + cardStatus + "\n"); //
-        }
-        return buffer.toString();
-    }
-
 }
 
 
