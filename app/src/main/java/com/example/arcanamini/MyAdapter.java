a9f8d9aa52fa3c20fad2e4b9f36ea8c766ebeb3f
nethen <72537484+nethen@users.noreply.github.com>
2023-03-03 14:34:24 -0800

    Updated cards

--- app/src/main/java/com/example/arcanamini/MyAdapter.java
+++ app/src/main/java/com/example/arcanamini/MyAdapter.java
@@ -1,15 +1,21 @@
 package com.example.arcanamini;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -32,7 +38,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(MyAdapter.MyViewHolder holder, int position) {
         String[] results = (list.get(position).toString()).split(",");
-        holder.nameButton.setText(results[0]);
+        holder.title.setText(results[0]);
 
     }
 
@@ -45,7 +51,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         public LinearLayout myLayout;
-        public Button nameButton;
+        public MaterialCardView mcardview;
+        public TextView title;
+        public TextView subtitle;
 
         Context context;
 
@@ -54,18 +62,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             myLayout = (LinearLayout) itemView;
 
 
-            nameButton = (Button) itemView.findViewById(R.id.cardNameButton);
-            nameButton.setOnClickListener(new View.OnClickListener() {
+            mcardview = itemView.findViewById(R.id.card_row);
+            title = itemView.findViewById(R.id.card_title);
+            subtitle = itemView.findViewById(R.id.card_subtitle);
+            mcardview.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     int position = getAdapterPosition(); //what item has been clicked
                     Toast.makeText(context,
-                            "You have clicked " + ((TextView) view.findViewById(R.id.cardNameButton)).getText().toString(),
+                            "You have clicked " + ((TextView) view.findViewById(R.id.card_row)).getText().toString(),
                             Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent (view.getContext(), CardDetailsActivity.class);
                     intent.putExtra ("ITEM_KEY", position);
-                    intent.putExtra ("ITEM_NAME", ((TextView) view.findViewById(R.id.cardNameButton)).getText().toString());
+                    intent.putExtra ("ITEM_NAME", ((TextView) view.findViewById(R.id.card_row)).getText().toString());
                     view.getContext().startActivity(intent);
                 }
             });
@@ -74,9 +84,5 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
         }
 
-        @Override
-        public void onClick(View view) {
-
-        }
     }
 }
