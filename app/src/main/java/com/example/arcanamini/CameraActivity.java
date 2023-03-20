136b51adb53648caf7257734891234e48e3ac5bc
jaspergass <115849451+jaspergass@users.noreply.github.com>
2023-03-20 14:26:59 -0700

    Choose from image gallery
    instead of starting the reflection by taking a picture, click a button to choose to add a picture or take one.

--- app/src/main/java/com/example/arcanamini/CameraActivity.java
+++ app/src/main/java/com/example/arcanamini/CameraActivity.java
@@ -1,12 +1,21 @@
 package com.example.arcanamini;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +25,20 @@ import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraProvider;
@@ -63,8 +80,8 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-//        previewView = findViewById(R.id.previewView);
-//        previewView.setVisibility(View.GONE);
+        previewView = findViewById(R.id.previewView);
+        previewView.setVisibility(View.GONE);
         imageViewCaptured = findViewById(R.id.imageViewCapturedImg);
         imageViewCaptured.setVisibility(View.GONE);
 
@@ -73,17 +90,13 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         buttonCaptureShow.setOnClickListener(this);
         saveButton = findViewById(R.id.newReflectionSaveButton);
         saveButton.setOnClickListener(this);
-        saveButton.setVisibility(View.GONE);
         reflectModeButton = findViewById(R.id.startReflectModeButton);
         reflectModeButton.setOnClickListener(this);
 
         //text
         notes = findViewById(R.id.newReflectionNotesTextView);
-        notes.setVisibility(View.GONE);
         time = findViewById(R.id.newReflectionTimeTextView);
-        time.setVisibility(View.GONE);
         noteContent = findViewById(R.id.newReflectionContentEditText);
-        noteContent.setVisibility(View.GONE);
 
         //database
         databaseHelper = new ReadDatabaseHelper(this);
@@ -104,20 +117,21 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         } else{
             Toast.makeText(this,"No data recieved", Toast.LENGTH_SHORT);
         }
-        //start camera on startup
-        Thread myThread = new Thread(new CameraThread());
-        myThread.start();
-
 
     }
     private class CameraThread implements Runnable
     {
         @Override
         public void run() {
-            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(camera_intent, img_id);
+//            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+//            File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+//            camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+//            startActivityForResult(camera_intent, img_id);
+
         }
     }
+
     private Executor getExecutor() {
         return ContextCompat.getMainExecutor(this);
     }
@@ -131,7 +145,8 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             public void run() {
                 try {
                     ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-//                    bindPreview(cameraProvider);
+                    bindPreview(cameraProvider);
+
 
                 } catch (ExecutionException | InterruptedException e) {
                     // This should never be reached.
@@ -140,24 +155,24 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         }, ContextCompat.getMainExecutor(this));
     }
 
-//    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-//
-//        cameraProvider.unbindAll();
-//
-//        Preview preview = new Preview.Builder()
-//                .build();
-//
-//        CameraSelector cameraSelector = new CameraSelector.Builder()
-//                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-//                .build();
-//
-//
-//        imageCapture = new ImageCapture.Builder()
-//                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-//                .build();
-//        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-//        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
-//    }
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        cameraProvider.unbindAll();
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+    }
 
     @Override
     public void onClick(View view) {
@@ -165,8 +180,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.buttonCaptureShow: {
 //                previewView.setVisibility(View.VISIBLE);
 //                capturePhoto();
-                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(camera_intent, img_id);
+
+                //start camera
+//                Thread myThread = new Thread(new CameraThread());
+//                myThread.start();
+                chooseImage(this);
                 break;
             }
             case R.id.newReflectionSaveButton: {
@@ -189,18 +207,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
-        //after taking an image:
-        imageViewCaptured.setImageBitmap(photo);
-        imageViewCaptured.setVisibility(View.VISIBLE);//show image
-        //show the following items:
-        notes.setVisibility(View.VISIBLE);
-        time.setVisibility(View.VISIBLE);
-        noteContent.setVisibility(View.VISIBLE);
-        saveButton.setVisibility(View.VISIBLE);
-    }
+
 
     private void capturePhoto() {
         long timeStamp = System.currentTimeMillis();
@@ -237,6 +244,76 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             }
         }
         return true;
+    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Bitmap photo = (Bitmap) data.getExtras().get("data");
+//        //after taking an image:
+//        imageViewCaptured.setImageBitmap(photo);
+//
+//    }
+
+    // function to let's the user to choose image from camera or gallery
+    private void chooseImage(Context context){
+        final CharSequence[] optionsMenu = {"Take Photo", "Choose from Gallery", "Exit" }; // create a menuOption Array
+        // create a dialog for showing the optionsMenu
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        // set the items in builder
+        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(optionsMenu[i].equals("Take Photo")){
+                    // Open the camera and get the photo
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                }
+                else if(optionsMenu[i].equals("Choose from Gallery")){
+                    // choose from  external storage
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+                }
+                else if (optionsMenu[i].equals("Exit")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageViewCaptured.setVisibility(View.VISIBLE);//show image
+
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        imageViewCaptured.setImageBitmap(selectedImage);
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imageViewCaptured.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
     }
 
 }
